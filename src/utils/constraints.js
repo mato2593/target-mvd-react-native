63caedff2329eff3547ef8e12976745b008fa097
Mathias Cabano <cabanomathias@gmail.com>
2018-08-14 20:32:11 -0300

    Add sign up form constraints

--- src/utils/constraints.js
+++ src/utils/constraints.js
@@ -10,5 +10,30 @@ export const login = {
   }
 };
 
+export const signUp = {
+  name: {
+    length: {
+      minimum: 3
+    },
+    presence: true
+  },
+  email: {
+    presence: true,
+    email: true
+  },
+  password: {
+    length: {
+      minimum: 6
+    },
+    presence: true
+  },
+  passwordConfirmation: {
+    equality: 'password'
+  },
+  gender: {
+    presence: true
+  }
+};
+
 export const validations = constraints =>
   data => validate(data.toJS(), constraints) || {};
